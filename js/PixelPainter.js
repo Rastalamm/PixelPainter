efa1955fc614805ea2de262dc83327c181132315
Judah Lamm <Judah.Lamm@gmail.com>
2015-06-13 19:56:10 -1000

    partial grid solution

---
+++ js/PixelPainter.js
@@ -0,0 +1,42 @@
+
+
+
+// window.onload = function(){
+
+
+//   (function general(){
+
+//    var container = document.createElement("div");
+//    container.id = 'main_container';
+//     document.body.appendChild(container);
+
+
+//     var containery = document.getElementById('main_container');
+
+
+//     var mainGridGenerator = function(x, y){
+
+//       for(var i = 1; i <= x; i++){
+//         var newColumn = document.createElement('div');
+//         newColumn.setAttribute('id', 'b' + i);
+//         newColumn.setAttribute('class', 'a_box column');
+//         containery.appendChild(newColumn);
+
+//          // for(var i = 1; i <= y; i++){
+//          //  var newRow = document.createElement('div');
+//          //  newRow.setAttribute('id', 'r' + i);
+//          //  newRow.setAttribute('class', 'a_box row');
+//          //  containery.appendChild(newRow);
+//          // }
+//       }
+
+//     };
+
+//     //mainGridGenerator(2,2);
+
+
+
+//   })();
+
+
+// }
