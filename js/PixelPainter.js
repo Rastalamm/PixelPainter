a7177972518a6aab45cfd77e66788cd728a6c64e
Judah Lamm <Judah.Lamm@gmail.com>
2015-06-15 15:38:49 -1000

    clicking of color pallet is working

--- js/PixelPainter.js
+++ js/PixelPainter.js
@@ -6,17 +6,20 @@ window.onload = function(){
     mainContainer.id = "gridtofill";
     document.body.appendChild(mainContainer);
 
+
+
     var grabMainContainer = document.getElementById('gridtofill');
 
     var mainGridGenerator = function(column, row){
 
       for(var i = 1; i <= row; i++){
         var aRow = document.createElement('div');
+        aRow.setAttribute('id', 'row' + i);
         aRow.setAttribute('class', 'row');
 
          for(var j = 1; j <= column; j++){
           var aBox = document.createElement('div');
-          aBox.setAttribute('id', 'r' + j);
+          aBox.setAttribute('id', 'box' + j);
           aBox.setAttribute('class', 'a_box');
           aRow.appendChild(aBox);
         }
@@ -37,43 +40,63 @@ window.onload = function(){
     var colorArray = ['yellow', 'blue', 'red', 'green', 'black', 'pink', 'orange', 'purple', 'white'];
 
     var colorArrayLength = colorArray.length;
-    console.log(colorArrayLength);
+
     var column = 0;
     var row = 0;
     var colorNumber = 0;
 
+    var colorSelected;
+    var colorEvent = document.getElementsByClassName('color_box');
 
     var colorSwatchGridGenerator = function(column, row){
-debugger;
+
       for(var i = 1; i <= row; i++){
         var aRow = document.createElement('div');
+        aRow.setAttribute('id', 'colorrow' + i);
         aRow.setAttribute('class', 'row');
 
-         for(var j = 0; j < column; j++){
-          var aBox = document.createElement('div');
-          aBox.setAttribute('id', 'r' + j);
-          aBox.style.backgroundColor = colorArray[colorNumber];
+         for(var j = 1; j <= column; j++){
+          var colorBox = document.createElement('div');
+          colorBox.setAttribute('id', 'colorbox' + j);
+          colorBox.style.backgroundColor = colorArray[colorNumber];
           colorNumber++;
-          aBox.setAttribute('class', 'a_box');
-          aRow.appendChild(aBox);
+          colorBox.setAttribute('class', 'color_box');
+          aRow.appendChild(colorBox);
         }
         grabSwatchContainer.appendChild(aRow);
-
       }
+
+      for(var w = 0; w < colorEvent.length; w++){
+        colorEvent[w].addEventListener("click", function(event){
+          colorSelected = event.target.style.backgroundColor;
+          console.log(colorSelected)
+        })
+      }
+
     };
 
-    // var colorSwatchGridNumber = function(colorArrayLength){
-    //     column = colorArrayLength / 2;
-    //     row = colorArrayLength / 2;
-    //   }
-    //  colorSwatchGridNumber(colorArrayLength);
+
 
 //build and input box that takes nums and places them here.
     mainGridGenerator(4,4);
 
-  colorSwatchGridGenerator(3, 3)
+    colorSwatchGridGenerator(3, 3)
+
+
+
+
+
+       // console.log(colorEvent);
+      //  console.log(colorEvent[0].style.backgroundColor);
+
+
+
+
+
 
 
 
 
 }
+
+
