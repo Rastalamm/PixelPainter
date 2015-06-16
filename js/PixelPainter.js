95b3568b4647b7fe8756876c80cb29302e87dad3
Judah Lamm <Judah.Lamm@gmail.com>
2015-06-15 14:31:47 -1000

    master grid is working, pixel grib is generated

--- js/PixelPainter.js
+++ js/PixelPainter.js
@@ -1,34 +1,79 @@
 
 window.onload = function(){
+
+
     var mainContainer = document.createElement("div");
-    mainContainer.id = "main_container";
+    mainContainer.id = "gridtofill";
     document.body.appendChild(mainContainer);
 
+    var grabMainContainer = document.getElementById('gridtofill');
 
-  (function general(){
+    var mainGridGenerator = function(column, row){
 
+      for(var i = 1; i <= row; i++){
+        var aRow = document.createElement('div');
+        aRow.setAttribute('class', 'row');
 
-    var grabMainContainer = document.getElementById('main_container');
-
-
-      var mainGridGenerator = function(x){
-
-        for(var i = 1; i <= x; i++){
-          var aRow = document.createElement('div');
-          //aRow.setAttribute('id', 'b' + i);
-          //newColumn.setAttribute('class');
-          grabMainContainer.appendChild(aRow);
-
-           for(var i = 1; i <= 1; i++){
-            var aBox = document.createElement('div');
-            aBox.setAttribute('id', 'r' + i);
-            aBox.setAttribute('class', 'a_box');
-            aRow.appendChild(aBox);
-          }
+         for(var j = 1; j <= column; j++){
+          var aBox = document.createElement('div');
+          aBox.setAttribute('id', 'r' + j);
+          aBox.setAttribute('class', 'a_box');
+          aRow.appendChild(aBox);
         }
-      };
-    mainGridGenerator(5);
-  })();
+        grabMainContainer.appendChild(aRow);
+
+      }
+    };
+
+
+
+    //Color swatch
+    var colorSwatch = document.createElement("div");
+    colorSwatch.id = "color_swatch";
+    document.body.appendChild(colorSwatch);
+
+    var grabSwatchContainer = document.getElementById('color_swatch');
+
+    var colorArray = ['yellow', 'blue', 'red', 'green', 'black', 'pink', 'orange', 'purple', 'white'];
+
+    var colorArrayLength = colorArray.length;
+    console.log(colorArrayLength);
+    var column = 0;
+    var row = 0;
+    var colorNumber = 0;
+
+
+    var colorSwatchGridGenerator = function(column, row){
+debugger;
+      for(var i = 1; i <= row; i++){
+        var aRow = document.createElement('div');
+        aRow.setAttribute('class', 'row');
+
+         for(var j = 0; j < column; j++){
+          var aBox = document.createElement('div');
+          aBox.setAttribute('id', 'r' + j);
+          aBox.style.backgroundColor = colorArray[colorNumber];
+          colorNumber++;
+          aBox.setAttribute('class', 'a_box');
+          aRow.appendChild(aBox);
+        }
+        grabSwatchContainer.appendChild(aRow);
+
+      }
+    };
+
+    // var colorSwatchGridNumber = function(colorArrayLength){
+    //     column = colorArrayLength / 2;
+    //     row = colorArrayLength / 2;
+    //   }
+    //  colorSwatchGridNumber(colorArrayLength);
+
+//build and input box that takes nums and places them here.
+    mainGridGenerator(4,4);
+
+  colorSwatchGridGenerator(3, 3)
+
+
 
 
 }
